99e3da0de628dcdcaaaaaf7d577740210e7c07ac
Michael Bryant <michael.bryant@kroger.com>
2019-07-01 22:23:51 -0400

    Added many examples, more slides. Now constructing slides inside of build-page.js

--- js/build-page.js
+++ js/build-page.js
@@ -1,27 +1,61 @@
 const fragments = [
-  'stroke-line',
-  'stroke-square',
-  'stroke-square-2',
-  'stroke-circle',
-  'fill-triangle',
-  'fill-triangle-2',
-  'fill-square',
-  'fill-square-2',
-  'fill-circle',
-  'fill-circle-2',
-  'draw-image',
-  'draw-image-2',
-  'event-loop',
-  'draw-image-webcam',
-  'edit-imagedata',
+  [
+    'intro',
+    'what-is-canvas',
+    'this-is-canvas',
+    'canvas-hard-to-see'
+  ],
+  [
+    'what-can-canvas-do',
+    '2D-examples',
+    '3D-examples',
+    'XR-examples'
+  ],
+  [
+    'context-api',
+    'stroke-line',
+    'stroke-square',
+    'stroke-square-2',
+    'stroke-circle',
+    'fill-triangle',
+    'fill-triangle-2',
+    'fill-square',
+    'fill-square-2',
+    'fill-circle',
+    'fill-circle-2',
+    'draw-image',
+    'draw-image-2',
+    'event-loop',
+    'draw-image-webcam',
+    'edit-imagedata',
+    'drawing-program',
+  ],
+  [
+    'webgl-api',
+    'webgl-draw-point',
+    'webgl-initShaders',
+    'three-cube',
+    'a-frame-cube'
+  ],
+  [
+    'canvas-vs-others',
+    'canvas-accessibility'
+  ]
 ]
-const previewSection = document.querySelector('#preview')
+const slides = document.querySelector('#slides')
 
-fragments.forEach(fileName => {
-  const section = document.createElement('section')
-  const fragmentLoader = document.createElement('html-fragment')
-  fragmentLoader.setAttribute('src', `fragments/${fileName}.html`)
+fragments.forEach(section => {
+  const sectionEl = document.createElement('section')
 
-  section.append(fragmentLoader)
-  previewSection.append(section)
+  section.forEach(fileName => {
+    const subSectionEl = document.createElement('section')
+    subSectionEl.id = fileName
+    const fragmentLoader = document.createElement('html-fragment')
+    fragmentLoader.setAttribute('src', `fragments/${fileName}.html`)
+
+    subSectionEl.append(fragmentLoader)
+    sectionEl.append(subSectionEl)
+  })
+
+  slides.append(sectionEl)
 })
